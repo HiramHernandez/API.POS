5084700f9ead291bd1b070a3f43cb763733c1ee0
HiramHernandez <hhg.mzt@gmail.com>
2023-03-23 14:57:00 -0600

    first commit

---
+++ POS.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace POS.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
